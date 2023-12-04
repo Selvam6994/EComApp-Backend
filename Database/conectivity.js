393a6d6250d885efff9e46d3a8f60c421f170b36
Selvakumar <selvamyoor@gmail.com>
2023-12-04 17:13:19 +0530

    conectivity changed

--- Database/conectivity.js
+++ Database/conectivity.js
@@ -1,20 +1,10 @@
-import mongoose from "mongoose";
 import dotenv from "dotenv";
 import { MongoClient } from "mongodb";
 
 dotenv.config();
 const dbConnectionString = process.env.MONGO_DB_URL;
 
+
 export const client = new MongoClient(dbConnectionString);
 
-const connectDB = async () => {
-  try {
-    const connection = await mongoose.connect(dbConnectionString);
-    console.log("connected to db");
-    return connection;
-  } catch (error) {
-    console.log(error);
-  }
-};
 
-export default connectDB;
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
-import connectDB from './Database/conectivity.js'
 import productRouter from './Routers/createProduct.route.js'
+import { client } from './Database/conectivity.js'
+
 
 dotenv.config()
 
@@ -10,11 +11,9 @@ const port = process.env.PORT
 const app = express()
 app.use(cors())
 app.use(express.json())
+await client.connect();
 
-console.log(connectDB())
 
 app.use('/api',productRouter)
 
-
 app.listen(port,()=>{console.log(`app connected to ${port}`)})
-
